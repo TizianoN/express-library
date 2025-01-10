1f9b6977111928d1ac6b0ca26925240eee66fb74
TizianoN <hello@tizianonicolai.com>
2025-01-10 10:26:52 +0100

    initial setup

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // # INIT EXPRESS
 const express = require("express");
 const app = express();
+const { APP_HOST, APP_PORT } = process.env;
+
+// # REGISTERING MIDDLEWARES
+app.use(express.json());
+app.use(express.static('public'));
+
+// # REGISTERING ROUTES
+const booksRouter = require("./routers/booksRouter");
+app.use("/books", booksRouter);
+
+// # ERROR HANDLERS
+const notFound = require("./middlewares/notFound");
+const errorsHandler = require("./middlewares/errorsHandler");
+
+app.use(errorsHandler);
+app.use(notFound);
 
 // # SERVER LISTENING
-app.listen(3000, () => {
-  console.log('Server listening at http://localhost:3000');
-})
+app.listen(APP_PORT, () => {
+  console.log(`Server listening at ${APP_HOST}:${APP_PORT}`);
+});
